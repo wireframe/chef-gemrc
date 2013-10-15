28443b79401fa5aff7ed2123a54d7a292d8967fc
Ryan Sonnek <ryan.sonnek@gmail.com>
2013-10-15 15:42:14 -0500

    bump version

--- metadata.rb
+++ metadata.rb
@@ -8,7 +8,7 @@ license           'Apache 2.0'
 description       'Configures rubygems to skip RI and RDoc installation.'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
-version           '0.0.1'
+version           '0.0.2'
 
 recipe            'gemrc::default', 'Configures rubygems to skip RI and RDoc installation.'
 
